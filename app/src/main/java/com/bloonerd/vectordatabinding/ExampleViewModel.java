f0e0f2ae1562a8eb46ace319f33fb4d46dd74873
MOHAMMED TSALIS <m.salis@go-jek.com>
2016-09-17 10:27:18 +0700

    add two way databinding in edit text

--- app/src/main/java/com/bloonerd/vectordatabinding/ExampleViewModel.java
+++ app/src/main/java/com/bloonerd/vectordatabinding/ExampleViewModel.java
@@ -3,12 +3,15 @@ package com.bloonerd.vectordatabinding;
 import android.content.Context;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.bloonerd.vectordatabinding.BR;
 
 public class ExampleViewModel extends BaseObservable {
 
     private String text;
+    private String inputText;
 
     public ExampleViewModel(String text) {
         this.text = text;
@@ -21,6 +24,31 @@ public class ExampleViewModel extends BaseObservable {
 
     public void setText(String text) {
         this.text = text;
-        notifyChange();
+        notifyPropertyChanged(BR.text);
     }
+
+    @Bindable
+    public String getInputText() {
+        return inputText;
+    }
+
+    public TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            inputText = s.toString();
+            notifyChange();
+        }
+    };
+
+
 }
